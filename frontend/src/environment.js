66e9eb669f34d4573c3539baf5236485e0ac7936
Priya Chandra <chandrapriya101@gmail.com>
2025-11-03 00:42:17 +0530

    Deployiing frontend

---
+++ frontend/src/environment.js
@@ -0,0 +1,6 @@
+let IS_PROD = true;
+const servers = IS_PROD
+  ? "https://joinin-w0wt.onrender.com"
+  : "http://localhost:8000"
+
+export default server;
--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -16,8 +16,9 @@ import StopScreenShareIcon from "@mui/icons-material/StopScreenShare";
 import ChatIcon from "@mui/icons-material/Chat";
 import Badge from "@mui/material/Badge";
 import { useNavigate } from "react-router-dom";
+import server from "../environment";
 
-const server_url = "http://localhost:8000";
+const server_url = server;
 
 var connections = {};
 
